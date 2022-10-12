624110e32d9a7a6dc2bd2c9bfda6b169865d3f66
9z9z9z9z <rolyan293@gmail.com>
2022-10-13 01:45:39 +0300

    last

--- lab_3_3_1.py
+++ lab_3_3_1.py
@@ -3,16 +3,19 @@ import datetime
 
 def nearest_date(*params):    # дд.мм.гггг
       today = datetime.date.today()
-      for data in params:
-            base = list()     # Массив дат
-            clone = list()    # Массив разностей дат
-            tmp = data.split(".")
-            for i in range(3):
-                  base.append(int(tmp[i]))
-                  clone.append(abs(today.day - int(tmp[i])) + abs(today.month - int(tmp[i])) * 30 + abs(today.year - int(tmp[i])) * 365)
+      today_int = today.day + today.month * 30 + today.year * 365
+      base = [0] * len(params)  # Массив дат
+      clone = list()  # Массив разностей дат
+
+      for k in range(len(params)):
+            tmp = params[k].split(".")
+            base[k] += int(tmp[0]) + int(tmp[1]) * 30 + int(tmp[2]) * 365
+            clone.append(base[k] - today_int)
+            print(clone[k])
+
+      base = (sorted(base, key=lambda num: clone))
+
+      print("end")
 
 
-            print("end")
-
-
-nearest_date("11.11.2001")
+nearest_date("11.11.2031", "11.11.1990")
