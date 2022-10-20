6b665d10da0e74765cdcd257347b936668a4d8a8
9z9z9z9z <rolyan293@gmail.com>
2022-10-20 12:40:22 +0300

    20.10.2022

--- lab_3_3_1.py
+++ lab_3_3_1.py
@@ -1,23 +1,19 @@
 import datetime
 from time import strftime
-
 import date_tools
 import date_calculator
 
 
-def nearest_date(*params):    # дд.мм.гггг
-      dates = [datetime.datetime.strptime(param, "%d.%m.%Y") for param in params]
-      dates.sort()
-      sorteddates = [datetime.datetime.strftime(ts, "%Y-%m-%d") for ts in dates]
+def nearest_date(*params):  # дд.мм.гггг
+    today = datetime.datetime.today()
+    dates = [datetime.datetime.strptime(param, "%d.%m.%Y") for param in params]
+    dates.append(today)
+    dates.sort()
+    index = dates.index(today)
+    try:
+          if
 
-      print(dates)
-      print(dates[1] - dates[0])
-      print(sorteddates)
-
-
-
-      print("end")
-
+    print("end")
 
 
 nearest_date("11.11.2001", "11.11.1990", "11.11.2003", "11.11.2002")
